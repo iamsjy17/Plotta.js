7a380e642f6bcc88e9dd0c9d64ad907b3b07c8b2
jewooSong <iamsjy0808@gmail.com>
2018-12-27 01:42:07 +0900

    babel, eslint Setting

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const eslint = require('gulp-eslint');
+
+const DIR = {
+  SRC: 'src',
+  DEST: 'dist',
+};
+
+const PATH = {
+  SRC: {
+    JS: `${DIR.SRC}/*.js`,
+  },
+  DEST: {
+    JS: `${DIR.DEST}/`,
+  },
+};
+
+gulp.task('default', () => {
+  gulp
+    .src(PATH.SRC.JS)
+    .pipe(eslint())
+    .pipe(eslint.format());
+
+  return gulp
+    .src(PATH.SRC.JS)
+    .pipe(babel())
+    .pipe(gulp.dest(PATH.DEST.JS));
+});
