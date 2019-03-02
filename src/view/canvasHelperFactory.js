9bda839b22cb509808c879fb5b6712adc6ffb271
jewooSong <iamsjy0808@gmail.com>
2019-03-03 00:51:40 +0900

    Add Comment (CanvasHelperFactory)

--- src/view/canvasHelperFactory.js
+++ src/view/canvasHelperFactory.js
@@ -2,6 +2,13 @@ import CanvasHelper from './canvasHelper';
 import OffscreenCanvasHelper from './osCanvasHelper';
 import Platform from '../platform/platform';
 
+/**
+ * @name CanvasHelperFactory
+ * @type Object
+ * @method Create
+ * Create a canvasHelper for the current platform with the CanvasHelperFactory.
+ */
+
 const CanvasHelperFactory = {
   Create(canvas, dpr) {
     if (!canvas) return null;
