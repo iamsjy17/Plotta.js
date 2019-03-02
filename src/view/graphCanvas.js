29acf0e7782535022c24fb006be8e52c4d78fb7c
jewooSong <iamsjy0808@gmail.com>
2019-03-03 00:43:54 +0900

    Add GraphCanvas Comment

--- src/view/graphCanvas.js
+++ src/view/graphCanvas.js
@@ -1,17 +1,33 @@
 /* eslint-disable no-param-reassign */
 import CanvasHelperFactory from './canvasHelperFactory';
 
+/**
+ * @name Plotta
+ * @type class
+ * @param {Object} canvasHelper
+ * Create a canvasHelper for the current platform with the CanvasHelperFactory.
+ *
+ * See function description
+ * @method Draw
+ */
+
 class GraphCanvas {
   constructor(canvas) {
     const dpr = window.devicePixelRatio || 1;
     const rect = canvas.getBoundingClientRect();
-    // Size of scale up Canvas
+    // Scale up the size of the canvas.
     canvas.width = rect.width * dpr;
     canvas.height = rect.height * dpr;
-
     this.canvasHelper = CanvasHelperFactory.Create(canvas, dpr);
   }
 
+  /**
+   * @name Draw
+   * @type function
+   * @param {Object} drawData ViewModel.DrawData
+   * @description
+   * Call the Draw function of the registered CanvasHelper.
+   */
   Draw(drawData) {
     if (drawData) this.canvasHelper.Draw(drawData);
   }
