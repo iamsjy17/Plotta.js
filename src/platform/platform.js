51c608f0ca83291259f30b75d5aab81cd89872c0
jewooSong <iamsjy0808@gmail.com>
2019-05-28 01:16:04 +0900

    feat: Add platform-specific types

--- src/platform/platform.js
+++ src/platform/platform.js
@@ -17,7 +17,7 @@ pf.PLATFORM_TYPE = {
 };
 
 pf.BROWSER_TYPE = {
-  lAZY: 0,
+  LAZY: 0,
   CHROME: 1,
   IE_11: 2,
   EDGE: 3,
@@ -30,12 +30,30 @@ pf.currentPlaform = (function () {
 }());
 
 pf.currentBrowser = (function () {
-  return pf.BROWSER_TYPE.CHROME;
+  const agt = navigator.userAgent.toLowerCase();
+  const name = navigator.appName;
+  let type = pf.BROWSER_TYPE.LAZY;
+
+  if (name === 'Microsoft Internet Explorer') {
+    type = pf.BROWSER_TYPE.IE;
+  } else if (agt.indexOf('tident') !== -1) {
+    type = pf.BROWSER_TYPE.IE_11;
+  } else if (agt.indexOf('edge/') !== -1) {
+    type = pf.BROWSER_TYPE.EDGE;
+  } else if (agt.indexOf('chrome') !== -1 || agt.indexOf('whale') !== -1) {
+    type = pf.BROWSER_TYPE.CHROME;
+  } else if (agt.indexOf('firefox') !== -1) {
+    type = pf.BROWSER_TYPE.FIREFOX;
+  } else if (agt.indexOf('safari') !== -1) {
+    type = pf.BROWSER_TYPE.SAFARI;
+  }
+
+  return type;
 }());
 
 pf.IsAvailableOffScreen = (function () {
   return (
-    pf.currentPlaform === pf.PLATFORM_TYPE.NODE || pf.currentBrowser === pf.BROWSER_TYPE.CHROME
+    pf.currentPlaform === pf.PLATFORM_TYPE.BROWSER && pf.currentBrowser === pf.BROWSER_TYPE.CHROME
   );
 }());
 
