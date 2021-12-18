8bf0e8ec3a073a5b7a87c5d3b06745272f389187
jacob.salad <jacob.salad@kakaocorp.com>
2021-12-18 19:23:15 +0900

    chore: Modify webpack config

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,10 +1,11 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 const config = {
   entry: {
-    plotta: ['@babel/polyfill', './src/plotta.js'],
+    plotta: ['@babel/polyfill', './src/plotta.ts'],
     testData: ['./src/demo/testData.js'],
   },
   output: {
